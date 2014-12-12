bc57e1121734ea73e20e705747b2eda622b69e69
Trey Terrell <trey.terrell@oregonstate.edu>
2014-12-12 14:54:45 -0800

    Fix test for CV.

--- spec/models/controlled_vocabulary_spec.rb
+++ spec/models/controlled_vocabulary_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe ControlledVocabulary do
+RSpec.describe ControlledVocabulary do
   it "should be an AT::Resource" do
     expect(ControlledVocabulary < ActiveTriples::Resource).to be true
   end
