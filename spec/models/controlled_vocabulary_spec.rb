d98736e8397e0560a04d5918ae707df628adda7c
Trey Terrell <trey.terrell@oregonstate.edu>
2014-12-15 10:10:19 -0800

    Basic data model.

--- spec/models/controlled_vocabulary_spec.rb
+++ spec/models/controlled_vocabulary_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe ControlledVocabulary do
   it "should be an AT::Resource" do
     expect(ControlledVocabulary < ActiveTriples::Resource).to be true
   end
+  it "should instantiate" do
+    expect{ControlledVocabulary.new}.not_to raise_error
+  end
 end
