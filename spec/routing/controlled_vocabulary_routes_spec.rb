44f9976ecf0dbc2e7631cd0c06b33294ec9a5906
Trey Terrell <trey.terrell@oregonstate.edu>
2014-12-17 10:29:37 -0800

    Test routes for Vocabularies.

--- spec/routing/controlled_vocabulary_routes_spec.rb
+++ spec/routing/controlled_vocabulary_routes_spec.rb
@@ -7,16 +7,19 @@ RSpec.describe "routes for Controlled Vocabularies" do
   it "routes /ns/bla" do
     expect(get("/ns/bla")).to route_to("controlled_vocabularies#show", :id => "bla")
   end
-  context "when given a resource" do
-    let(:id) {"bla/bla"}
-    let(:resource) do
-      r = ControlledVocabulary.new
-      allow(r).to receive(:id).and_return(id)
-      allow(r).to receive(:persisted?).and_return(true)
-      r
+  [Vocabulary, ControlledVocabulary].each do |klass|
+    context "when given a #{klass}" do
+      let(:id) {"bla/bla"}
+      let(:resource) do
+        r = klass.new
+        allow(r).to receive(:id).and_return(id)
+        allow(r).to receive(:persisted?).and_return(true)
+        r
+      end
+      it "should route to its id preserving slashes" do
+        expect(controlled_vocabulary_path(resource)).to eq "/ns/bla/bla"
+      end
     end
-    it "should route to its id preserving slashes" do
-      expect(controlled_vocabulary_path(resource)).to eq "/ns/bla/bla"
-    end
+
   end
 end
