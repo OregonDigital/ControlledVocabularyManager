8f775da864b94e4b31e86cc7f8ad01121d865a5e
Trey Terrell <trey.terrell@oregonstate.edu>
2014-12-18 10:46:58 -0800

    Vocabulary Creator

---
+++ app/services/vocabulary_creator.rb
@@ -0,0 +1,41 @@
+class VocabularyCreator
+  def self.call(params)
+    new(params)
+  end
+
+  attr_accessor :params, :result
+  def initialize(params)
+    @params = params
+    @result = false
+  end
+
+  def vocabulary
+    populate_vocabulary if @vocabulary.nil?
+    @vocabulary
+  end
+
+  private
+
+  def populate_vocabulary
+    @vocabulary = Vocabulary.new(params.delete(:id))
+    check_existence
+    set_attributes
+    persist_vocabulary
+  end
+
+  def check_existence
+    vocabulary.errors.add(:id, "already exists in the repository") if vocabulary.persisted?
+  end
+
+  def persist_vocabulary
+    # This return is a hack. Checking #valid? clears out errors and re-runs
+    # validations, which means the above doesn't work. Need a better way to do
+    # this maybe. A decorator with validators maybe?
+    return unless vocabulary.errors.empty?
+    @result = vocabulary.persist!(:validate => true)
+  end
+
+  def set_attributes
+    vocabulary.attributes = params
+  end
+end
---
+++ spec/services/vocabulary_creator_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe VocabularyCreator do
+  let(:params) do
+    {
+      :id => id,
+      :label => label,
+      :comment => comment
+    }
+  end
+  let(:id) { "Creator" }
+  let(:label) { ["Test Label"] }
+  let(:comment) { ["Test Comment"] }
+  subject { VocabularyCreator.call(params) }
+  before do
+    stub_repository
+  end
+  
+  describe ".call.vocabulary" do
+    let(:result) { subject.vocabulary }
+    context "when given good parameters" do
+      it "should return a Vocabulary" do
+        expect(result).to be_kind_of Vocabulary
+      end
+      it "should have a good id" do
+        expect(result.rdf_subject.to_s).to eq "http://opaquenamespace.org/ns/Creator"
+      end
+      it "should set attributes" do
+        expect(result.label).to eq label
+        expect(result.comment).to eq comment
+      end
+      it "should be persisted" do
+        expect(result).to be_persisted
+      end
+      it "should have a true result" do
+        result
+        expect(subject.result).to eq true
+      end
+    end
+    context "when given an already existing vocabulary" do
+      before do
+        Vocabulary.new(id).persist!
+      end
+      it "should have errors" do
+        expect(result.errors).not_to be_empty
+      end
+      it "should have a false result" do
+        result
+        expect(subject.result).to eq false
+      end
+    end
+    context "when given bad parameters" do
+      let(:id) {nil}
+      it "should not be valid" do
+        expect(result).not_to be_valid
+      end
+      it "should not be persisted" do
+        expect(result).not_to be_persisted
+      end
+      it "should have a false result" do
+        result
+        expect(subject.result).to eq false
+      end
+    end
+  end
+end
