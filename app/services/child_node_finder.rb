f8b516053bd0efeb27d424c27e0fe31928b85032
Trey Terrell <trey.terrell@oregonstate.edu>
2015-02-23 11:09:07 -0800

    Extract responsibilities to classes.

--- app/services/child_node_finder.rb
+++ app/services/child_node_finder.rb
@@ -14,8 +14,51 @@ class ChildNodeFinder
 
   private
 
-  def children_solutions
-    @children_solutions ||= query.each_solution.to_a
+  def children_graph
+    ChildQuery.new(sparql_client, vocabulary.rdf_subject).run
+  end
+
+  def children_terms
+    GraphToTerms.new(children_graph).run
+  end
+
+  def sparql_client
+    vocabulary.repository.query_client
+  end
+end
+
+class GraphToTerms < Struct.new(:graph)
+  def run
+    graph.each_statement.group_by(&:subject).map do |subject, triples|
+      build_term(subject, triples)
+    end
+  end
+
+  private
+
+  def build_term(subject, triples)
+    t = Term.new(subject)
+    t.insert(*triples)
+    t
+  end
+
+end
+
+class ChildQuery < Struct.new(:sparql_client, :parent_uri)
+  def run
+    graph
+  end
+
+  private
+
+  def graph
+    g = RDF::Graph.new
+    g.insert(*triples)
+    g
+  end
+
+  def solutions
+    query.each_solution.to_a
   end
 
   def query
@@ -23,27 +66,15 @@ class ChildNodeFinder
   end
 
   def query_filter
-    "STRSTARTS(STR(?s), \"#{vocabulary.rdf_subject}/\")"
+    "STRSTARTS(STR(?s), \"#{parent_uri}/\")"
   end
 
-  def children_triples
-    @children_triples ||= children_solutions.map{|x| to_triple(x)}
+  def triples
+    solutions.map{ |x| to_triple(x) }
   end
 
   def to_triple(solution)
     hsh = solution.to_hash
     RDF::Statement.from([hsh[:s], hsh[:p], hsh[:o]])
-  end
-
-  def children_terms
-    children_triples.group_by(&:subject).map do |subject, triples|
-      t = Term.new(subject)
-      t.insert(*triples)
-      t
-    end
-  end
-
-  def sparql_client
-    vocabulary.repository.query_client
   end
 end
