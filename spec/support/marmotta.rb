64e8c2bd653a81e9aca58c386a3a05b68aff09a3
Trey Terrell <trey.terrell@oregonstate.edu>
2015-02-23 11:46:46 -0800

    Marmotta was not clearing before.

--- spec/support/marmotta.rb
+++ spec/support/marmotta.rb
@@ -1,6 +1,9 @@
 MARMOTTA_REPO ||= RDF::Marmotta.new("http://localhost:8983/marmotta/")
 RSpec.configure do |config|
   config.before do
-    MARMOTTA_REPO.clear!
+    # TODO: We NEED to fix this.
+    while MARMOTTA_REPO.count > 0
+      MARMOTTA_REPO.clear!
+    end
   end
 end
