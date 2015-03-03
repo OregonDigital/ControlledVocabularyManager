5d03b37aa71a1762df5b1301406a286de37aa176
Trey Terrell <trey.terrell@oregonstate.edu>
2015-03-03 08:48:23 -0800

    load_term was no longer useful.

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,11 +1,11 @@
 class TermsController < ApplicationController
-  before_filter :load_term, :only => [:show]
   before_filter :load_vocabulary, :only => [:new, :create]
   delegate :term_form, :edit_term_form, :term_repository, :vocabulary_repository, :to => :injector
   rescue_from ActiveTriples::NotFound, :with => :render_404
   skip_before_filter :check_auth, :only => [:show]
 
   def show
+    @term = term_repository.find(params[:id])
     respond_to do |format|
       format.html
       format.nt { render body: @term.full_graph.dump(:ntriples), :content_type => Mime::NT }
@@ -44,10 +44,6 @@ class TermsController < ApplicationController
 
   def injector
     @injector ||= TermInjector.new(params)
-  end
-
-  def load_term
-    @term = term_repository.find(params[:id])
   end
 
   def load_vocabulary
