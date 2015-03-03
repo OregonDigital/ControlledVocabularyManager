557c005f9d4464a9510011eb8efb2feeb48596ff
Trey Terrell <trey.terrell@oregonstate.edu>
2015-03-03 08:59:56 -0800

    Before filters for loading variables was a mistake.

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,11 +1,11 @@
 class TermsController < ApplicationController
-  before_filter :load_vocabulary, :only => [:new, :create]
   delegate :term_form, :edit_term_form, :term_repository, :vocabulary_repository, :to => :injector
   rescue_from ActiveTriples::NotFound, :with => :render_404
   skip_before_filter :check_auth, :only => [:show]
 
   def show
-    @term = term_repository.find(params[:id])
+    @term = find_term
+
     respond_to do |format|
       format.html
       format.nt { render body: @term.full_graph.dump(:ntriples), :content_type => Mime::NT }
@@ -15,9 +15,12 @@ class TermsController < ApplicationController
 
   def new
     @term = term_form
+    @vocabulary = find_vocabulary
   end
 
   def create
+    @vocabulary = find_vocabulary
+
     if term_form.save
       redirect_to term_path(:id => term_form.id)
     else
@@ -46,8 +49,12 @@ class TermsController < ApplicationController
     @injector ||= TermInjector.new(params)
   end
 
-  def load_vocabulary
-    @vocabulary = vocabulary_repository.find(params[:vocabulary_id])
+  def find_term
+    term_repository.find(params[:id])
+  end
+
+  def find_vocabulary
+    vocabulary_repository.find(params[:vocabulary_id])
   end
 
   def render_404
