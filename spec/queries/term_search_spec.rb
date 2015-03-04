a34a55e2c7e4cbc888fc5fffce0047fde7e4a8c1
Trey Terrell <trey.terrell@oregonstate.edu>
2015-03-03 16:23:55 -0800

    Test for returning multiple types.

--- spec/queries/term_search_spec.rb
+++ spec/queries/term_search_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe TermSearch do
   let(:sparql_client) { Term.new.repository.query_client }
 
   describe "#search" do
+
+    def build_relevant_term(factory, id, query)
+      t = factory.new(id)
+      t.label = "afadf#{query}afadf"
+      t.persist!
+      t
+    end
     let(:query) { "banana" }
     let(:results) { subject.search(query) }
     context "when there are no results" do
@@ -14,10 +21,7 @@ RSpec.describe TermSearch do
     end
     context "when there are results" do
       let(:term) do
-        t = Term.new("vocab/id")
-        t.label = "ababananarama"
-        t.persist!
-        t
+        build_relevant_term(Term, "vocab/id", query)
       end
       before do
         term
@@ -25,16 +29,22 @@ RSpec.describe TermSearch do
       it "should return it" do
         expect(results).to eq [term]
       end
+      context "with terms and vocabularies" do
+        before do
+          build_relevant_term(Vocabulary, "id", query)
+          build_relevant_term(Term, "id/term", query)
+        end
+        it "should return terms and vocabularies" do
+          expect(results.map(&:class)).to eq [Term, Vocabulary, Term]
+        end
+      end
       context "which are vocabularies" do
         let(:term) do
-          t = Vocabulary.new("id")
-          t.label = "banana"
-          t.persist!
-          t
+          build_relevant_term(Vocabulary, "vocab", query)
         end
         it "should return a vocabulary" do
           expect(results).to eq [term]
-          expect(results.first).to be_kind_of Vocabulary
+          expect(results.first).to be_instance_of Vocabulary
         end
       end
     end
