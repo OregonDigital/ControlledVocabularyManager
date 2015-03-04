0911206f36f03a7ad01093d4e25f6350a2293a53
Trey Terrell <trey.terrell@oregonstate.edu>
2015-03-03 16:11:29 -0800

    Extract collaborators for AllVocabsQuery.

---
+++ lib/solutions_to_graph.rb
@@ -0,0 +1,20 @@
+class SolutionsToGraph < Struct.new(:solutions)
+  def graph
+    g = RDF::Graph.new
+    g.insert(*triples)
+    g
+  end
+
+  private
+
+  def triples
+    solutions.map{ |x| to_triple(x) }
+  end
+
+  def to_triple(solution)
+    hsh = solution.to_hash
+    RDF::Statement.from([hsh[:s], hsh[:p], hsh[:o]])
+  end
+
+end
+
