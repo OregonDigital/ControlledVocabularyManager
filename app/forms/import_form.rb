7f631b337995638386961b1d248e053ad4b7e0d1
Jeremy Echols <jechols@uoregon.edu>
2015-03-09 19:33:45 +0000

    Use ActiveModel::Model for FBO

--- app/forms/import_form.rb
+++ app/forms/import_form.rb
@@ -1,24 +1,10 @@
 class ImportForm
-  extend ActiveModel::Naming
-  include ActiveModel::Conversion
-  include ActiveModel::Validations
-
-  ATTRIBUTES = [:url, :preview]
-  attr_accessor *ATTRIBUTES
+  include ActiveModel::Model
+  attr_accessor :url, :preview
 
   validates_presence_of :url
 
-  def persisted?
-    false
-  end
-
   def preview?
     preview == "1"
-  end
-
-  def initialize(attributes = {})
-    ATTRIBUTES.each do |attribute|
-      send("#{attribute}=", attributes[attribute])
-    end
   end
 end
