5ce70d0ad6664eb3efc2db12018fc30530d8b321
Trey Terrell <trey.terrell@oregonstate.edu>
2015-03-09 08:47:16 -0700

    Document marmotta bug.

--- spec/queries/all_vocabs_query_spec.rb
+++ spec/queries/all_vocabs_query_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 RSpec.describe AllVocabsQuery do
   describe ".call" do
     context "when vocabularies exist" do
-      before do
-        stub_repository
-      end
       let(:vocabulary) { SetsIssued.new(Vocabulary.new("bla")) }
       let(:sparql_client) { vocabulary.repository.query_client }
       let(:repository) { VocabularyInjector.new.vocabulary_repository }
@@ -19,6 +16,23 @@ RSpec.describe AllVocabsQuery do
       it "should not return terms" do
         repository.new("2/1").persist!
         expect(described_class.call(sparql_client, repository)).to eq [vocabulary.__getobj__]
+      end
+      context "when given a limit" do
+        subject { described_class.call(sparql_client, repository, :limit => 1) }
+        let(:vocabulary_2) { SetsIssued.new(Vocabulary.new("bla2")) }
+        before do
+          vocabulary_2.persist!
+        end
+        it "should only return that many" do
+          expect(subject).to eq [vocabulary.__getobj__]
+        end
+        context "when given an offset" do
+          # This is a marmotta bug - it NEEDS a non-0 limit to use offset.
+          subject { described_class.call(sparql_client, repository, :offset => 1, :limit => 10000000) }
+          it "should return that one" do
+            expect(subject).to eq [vocabulary_2.__getobj__]
+          end
+        end
       end
     end
   end
