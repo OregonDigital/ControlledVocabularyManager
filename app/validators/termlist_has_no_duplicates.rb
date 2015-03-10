db6a76e8803ead1ac85e2de675c7ec7df650f733
Jeremy Echols <jechols@uoregon.edu>
2015-03-10 15:57:56 +0000

    Simplify termlist dupe checking validation

--- app/validators/termlist_has_no_duplicates.rb
+++ app/validators/termlist_has_no_duplicates.rb
@@ -1,21 +1,14 @@
 # Verifies that all terms in a list are unique within the list
 class TermlistHasNoDuplicates < ActiveModel::Validator
   def validate(record)
-    @uris_seen = Hash.new(0)
-    record.terms.each do |term|
-      if seen(term) == 2
-        record.errors.add(:base, "%s already exists in the list" % term_uri(term))
-      end
+    duplicate_subjects(record).each do |subject|
+      record.errors.add(:base, "%s already exists in the list" % subject)
     end
   end
 
   private
 
-  def seen(term)
-    @uris_seen[term_uri(term)] += 1
-  end
-
-  def term_uri(term)
-    term.rdf_subject.to_s
+  def duplicate_subjects(record)
+    record.terms.group_by(&:rdf_subject).select{|k, v| v.length > 1}.keys
   end
 end
