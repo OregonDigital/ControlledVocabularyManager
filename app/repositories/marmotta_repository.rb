e4cdbc2fd324e9983d25cff6ef6a6e633b8e46d4
Trey Terrell <trey.terrell@oregonstate.edu>
2015-06-18 16:15:39 -0700

    Instantiate graph.

--- app/repositories/marmotta_repository.rb
+++ app/repositories/marmotta_repository.rb
@@ -14,7 +14,7 @@ class MarmottaRepository
   end
 
   def <<(stuff)
-    graph = (RDF::Graph << marmotta_resource.get)
+    graph = (RDF::Graph.new << marmotta_resource.get)
     graph << stuff
     marmotta_resource.post(graph)
     true
