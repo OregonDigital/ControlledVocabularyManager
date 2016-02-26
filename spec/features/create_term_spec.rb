58e7533a527e09b4f01fa12048af01f5568b5940
lsato <lsato@uoregon.edu>
2016-02-26 14:23:26 -0800

    adding auth

--- spec/features/create_term_spec.rb
+++ spec/features/create_term_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.feature "Creating a vocabulary & term", :js => true do
   background do
     allow_any_instance_of(ApplicationController).to receive(:check_auth).and_return(true)
-    @vocabulary_page = VocabularyIndexPage.new
-    visit vocabularies_path
   end
   scenario "succesfully creating a term" do
-    expect(@vocabulary_page).to be_visible
-    vocabulary_create_page = @vocabulary_page.click_create
+    vocabulary_create_page = VocabularyCreatePage.new
+    visit "/vocabularies/new"
 
     expect(vocabulary_create_page).to be_visible
     vocabulary_show_page = vocabulary_create_page.create
     
     expect(get_vocab_statement_list[3].object.language).to eq :aa
     expect(vocabulary_show_page).to be_visible
-    term_create_page = vocabulary_show_page.click_create_term
+
+    term_create_page = TermCreatePage.new("TestVocab")
+    visit "/vocabularies/TestVocab/new"
 
     expect(term_create_page).to be_visible
     term_show_page = term_create_page.create
