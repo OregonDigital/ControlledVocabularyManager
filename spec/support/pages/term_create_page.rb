b95346b87122a35cdf1ec90d4f477af46016b7bf
Josh Gum <josh.gum@oregonstate.edu>
2016-03-04 15:03:28 -0800

    fixes a race condition that poltergeist has on testing our create vocab and term functionality

--- spec/support/pages/term_create_page.rb
+++ spec/support/pages/term_create_page.rb
@@ -9,7 +9,8 @@ class TermCreatePage < Struct.new(:vocabulary_id)
     fill_in "ID", :with => "banana"
     fill_in "vocabulary[label][]", :with => "Test label"
     fill_in "vocabulary[comment][]", :with => "Test comment"
-    find(:xpath, "//input[@name = 'commit']").trigger("click")
+    find(:xpath, "//*[@name='commit']").trigger('click')
+    sleep 2
     TermShowPage.new(id)
   end
 
