58efd36dd66a0bf6eb4e25fe0e4d841129af31ac
Josh Gum <josh.gum@oregonstate.edu>
2016-04-04 14:03:34 -0700

    fixes language bug and feature spec, introduces UI spec for default language

---
+++ spec/ui/default_language_select_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Using a language SELECT with a default language set", :js => true do
+  background do
+    allow_any_instance_of(ApplicationController).to receive(:check_auth).and_return(true)
+  end
+
+  scenario "adding another label defaults lanuage select to english" do
+    visit "/vocabularies/new"
+    within('.vocabulary_label') do
+      find(".language-select option[value='es']").select_option
+      expect(find(".language-select option[value='es']")).to be_selected
+      click_button("Add")
+    end
+    within('.vocabulary_label ul.listing li:nth-child(2)') do
+      expect(find(".language-select option[value='en']")).to be_selected
+    end
+  end
+end
