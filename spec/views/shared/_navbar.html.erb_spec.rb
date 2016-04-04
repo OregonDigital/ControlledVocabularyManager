2d89f17cf62c4bc29cf56ce41b2057d15ce336cb
luisgreg99 <luisram90_101@hotmail.com>
2016-04-04 11:54:31 -0700

    hide import external rdf link when not logged in

--- spec/views/shared/_navbar.html.erb_spec.rb
+++ spec/views/shared/_navbar.html.erb_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "shared/_navbar.html.erb" do
-  before do
-    render
-  end
   it "should have a link to vocabularies" do
+    render
     expect(rendered).to have_link("Vocabularies", :href => vocabularies_path)
+  end
+  context "when logged in" do
+    before do
+      session[:authorized] = true
+    end
+    it "should display a link to import external RDF  " do
+      render
+      expect(rendered).to have_link "Import External RDF", :href => "/import_rdf"
+    end
+  end
+  context "when not logged in" do
+    it "should not display link to import external RDF" do
+      render
+      expect(rendered).to_not have_link "Import External RDF", :href => "/import_rdf"
+    end
   end
 end
