6be18bd0d6c32c2978448fa2fb088ecf4256bc88
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-05-12 11:12:57 -0700

    add sub property of and range properties

--- app/models/predicate.rb
+++ app/models/predicate.rb
@@ -1,4 +1,6 @@
 class Predicate < Term
   configure :type => RDF::URI("http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate")
+  property :sub_property_of, :predicate => RDF::RDFS.subPropertyOf
+  property :range, :predicate => RDF::RDFS.range
 
 end
