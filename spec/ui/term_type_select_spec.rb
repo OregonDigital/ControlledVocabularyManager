6e443c14684a983994ed31d628b1d03d37b96df9
lsato <lsato@uoregon.edu>
2016-05-12 15:39:57 -0700

    adding setup_git to tests

--- spec/ui/term_type_select_spec.rb
+++ spec/ui/term_type_select_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 require 'corporate_name'
-
+require 'support/test_git_setup'
 RSpec.feature "Using the term type SELECT", :js => true do
+  include TestGitSetup
   background do
     allow_any_instance_of(AdminController).to receive(:require_admin).and_return(true)
   end
 
   scenario "hide fields which aren't specifically configured as visible for the model" do
+    setup_git
     vocabulary_create_page = VocabularyCreatePage.new
     visit "/vocabularies/new"
     vocabulary_create_page.create
@@ -19,5 +21,6 @@ RSpec.feature "Using the term type SELECT", :js => true do
     end
     expect(page).to have_selector("label.term_alternate_name", visible: false)
     expect(page).to have_selector("div.term_alternate_name", visible: false)
+    FileUtils.rm_rf(ControlledVocabularyManager::Application::config.rugged_repo)
   end
 end
