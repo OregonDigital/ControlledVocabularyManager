30630c1ec6b279aa71251057ad0e0ee2f4d5bf51
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-05-17 14:29:01 -0700

    Added name to users.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,7 +40,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.for(:sign_up) << :institution
+     devise_parameter_sanitizer.for(:sign_up) << [:institution, :name]
    end
 
   # If you have extra params to permit, append them to the sanitizer.
