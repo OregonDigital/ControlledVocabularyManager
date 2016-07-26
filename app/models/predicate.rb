e14b9fff0314de6ce5fdd836c78238adf1572004
Ryan Wick <ryan.wick@oregonstate.edu>
2016-07-26 10:26:04 -0700

    Additional field configuration.

--- app/models/predicate.rb
+++ app/models/predicate.rb
@@ -4,4 +4,21 @@ class Predicate < Term
   property :range, :predicate => RDF::RDFS.range
   property :domain, :predicate => RDF::RDFS.domain
 
+  def self.option_text
+    "Predicate"
+  end
+
+  def self.uri
+    self.type.to_s
+  end
+
+  def self.visible_form_fields
+    %w[label alternate_name date comment is_replaced_by see_also is_defined_by same_as modified issued title publisher sub_property_of range domain]
+  end
+
+  # Update the fields method with any new properties added to this model
+  def fields
+    [:sub_property_of, :range, :domain] | super
+  end
+
 end
