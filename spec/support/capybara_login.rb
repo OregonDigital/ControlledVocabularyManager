0a7a5689af841529f0b0d5c36e5aa66e84479879
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-09-07 10:43:52 -0700

    Fixes deprications and intermittent failures

---
+++ spec/support/capybara_login.rb
@@ -0,0 +1,7 @@
+def capybara_login(user)
+  visit "users/sign_in"
+  fill_in 'user_email', with: user[:email]
+  fill_in 'user_password', with: user[:password]
+  click_button 'Log in'
+  expect(page).to have_content('Jane Admin')
+end
