3d405a26862db65869bd63925c132a30a44ea2b0
lsato <lsato@uoregon.edu>
2016-10-24 11:01:23 -0700

    dont convert string uris

--- spec/values/maybe_uri.rb
+++ spec/values/maybe_uri.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe MaybeURI do
   let(:valid_uri) {"http://example.com"}
   let(:invalid_uri) {"http:example.com"}
+  let(:should_be_a_string) {"blah blah http://example.com blah"}
 
   describe "#value" do
     it "should convert a string if it looks like a URI" do
@@ -13,13 +14,16 @@ RSpec.describe MaybeURI do
       expect(MaybeURI.new(invalid_uri).value).to eql(invalid_uri)
     end
 
-    it "shouldn't convert non-strings" do
+    it "should not convert non-strings" do
       fake = double("fake")
       allow(fake).to receive(:to_s).and_return(valid_uri)
       expect(MaybeURI.new(fake.to_s).value).to eql(RDF::URI(valid_uri))
 
       # Sanity check
       expect(MaybeURI.new(fake).value).to eql(fake)
+    end
+    it "should not convert a uri that has stringy stuff around it" do
+      expect(MaybeURI.new(should_be_a_string).value).to eql(should_be_a_string)
     end
   end
 end
