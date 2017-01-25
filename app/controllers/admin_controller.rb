f1d784c3c195eb6a59b2f1e8ba49fac078c1ef7b
Brandon Straley <straleyb@onid.oregonstate.edu>
2017-01-25 15:37:45 -0800

    Allow the show page to be accessed by all.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  #Except show and index is important for fetching ntriples and other data types.
   before_filter :require_admin, :except => [:index, :show]
   
   def index
