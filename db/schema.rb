c22e8a38e1eb19bf672575d0dd24ced1eb49e881
lsato <lsato@uoregon.edu>
2017-05-18 13:56:15 -0700

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516223004) do
+ActiveRecord::Schema.define(version: 20170515170717) do
 
-  create_table "users", force: true do |t|
+  create_table "git_commits", force: :cascade do |t|
+    t.string   "term_id"
+    t.string   "unmerged_id"
+    t.string   "commit_ids"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
