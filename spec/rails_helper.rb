dd82da66c9b6ebdbe6851e84331225f8d7085910
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-05-01 13:18:07 -0700

    config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -84,5 +84,8 @@ RSpec.configure do |config|
   end
   config.before(:suite) do
     Capybara.current_driver = Capybara.javascript_driver
+  end
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, timeout: 1.minute)
   end
 end
