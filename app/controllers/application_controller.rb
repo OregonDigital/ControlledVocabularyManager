0c98063ecb0868d518cde102475b1cd8806cfc50
Brandon Straley <straleyb@onid.oregonstate.edu>
2018-10-18 12:53:41 -0700

    remove layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  layout 'blacklight'
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
