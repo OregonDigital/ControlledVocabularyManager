421b2de6df807562ef0d7d891e50c833550e2ffe
Ryan Wick <ryan.wick@oregonstate.edu>
2018-11-01 14:44:49 -0700

    Don't run sunspot initializer in test env.

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,4 +1,6 @@
-config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'sunspot.yml')).result)[Rails.env].with_indifferent_access
-Sunspot.config.solr.url = config['solr']['hostname']
-Sunspot.config.pagination.default_per_page = config['solr']['pagination_default_per_page'] || 30
-Sunspot.config.indexing.default_batch_size = config['solr']['index_default_batch_size'] || 30
+unless Rails.test?
+  config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'sunspot.yml')).result)[Rails.env].with_indifferent_access
+  Sunspot.config.solr.url = config['solr']['hostname']
+  Sunspot.config.pagination.default_per_page = config['solr']['pagination_default_per_page'] || 30
+  Sunspot.config.indexing.default_batch_size = config['solr']['index_default_batch_size'] || 30
+end
