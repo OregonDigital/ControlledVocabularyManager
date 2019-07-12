c58b995e4e5837518719d4101373882a3424e387
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2019-07-12 10:30:54 -0700

    Rehydration fixes (#548)
    * update to make all fields be interpreted as strings
    
    * updates to further refine rehydration. BG needs attention
    
    * remove binding pry
    
    * remove pid

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20160516223004) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
