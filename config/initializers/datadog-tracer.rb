90676fbbe4afb4d00ff6c0fd4774cf66c012b12b
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2019-10-02 16:18:28 -0700

    removed dd tracer initializer

--- config/initializers/datadog-tracer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-if %w[production staging].include? Rails.env
-  Datadog.configure do |c|
-    c.use :rails, service_name: "controlled-vocabulary-manager-#{Rails.env}"
-    c.use :http, service_name: "controlled-vocabulary-manager-#{Rails.env}-http"
-  end
-end
