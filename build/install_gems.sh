f078dac2770bfffabad0f6010f41a0d5a086c732
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-08-18 12:56:27 -0700

    added log output for non-production

--- build/install_gems.sh
+++ build/install_gems.sh
@@ -4,5 +4,6 @@ if [ "${RAILS_ENV}" = 'production' ]; then
   echo "Bundle install without development or test gems."
   bundle install --without development test -j $(nproc)
 else
+  echo "Bundle install with all gems."
   bundle install -j $(nproc)
 fi
