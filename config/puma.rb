3f83a94cebb320e0413c793c169cbaa705ea5869
Ryan Ordway <ryan.ordway@oregonstate.edu>
2021-12-02 13:52:30 -0800

    set worker_threads from PUMA_WORKER_TIMEOUT, default 120s

--- config/puma.rb
+++ config/puma.rb
@@ -17,6 +17,9 @@ port        ENV.fetch('PORT') { 3000 }
 #
 environment ENV.fetch('RAILS_ENV') { 'development' }
 
+# Specifies the worker timeouts for puma workers
+worker_timeout ENV.fetch('PUMA_WORKER_TIMEOUT') { '120' }
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
